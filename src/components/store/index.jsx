3e3ac53191787ec2edfc857db0087520d3d4cb88
zeeyan23 <zeeyanraza444@gmail.com>
2022-06-22 16:08:34 +0530

    added

--- src/components/store/index.jsx
+++ src/components/store/index.jsx
@@ -1,24 +1,35 @@
 import {createStore} from 'redux';
 
-function counterReducer(state= {counter:0},action){
+const initialState={counter:0,showCounter:true}
+function counterReducer(state= initialState,action){
     if(action.type==='increment'){
         return{
-            counter:state.counter+1
+            counter:state.counter+1,
+            showCounter:state.showCounter
         }
     }
     if(action.type==='increase'){
         return{
             counter:state.counter+action.amount,
+            showCounter:state.showCounter
         }
     }
     if(action.type==='decrement'){
         return{
-            counter:state.counter-1
+            counter:state.counter-1,
+            showCounter:state.showCounter
         }
     }
     if(action.type==='decrease'){
         return{
             counter:state.counter-action.amount,
+            showCounter:state.showCounter
+        }
+    }
+    if(action.type==='toggle'){
+        return{
+            showCounter:!state.showCounter,
+            counter:state.counter
         }
     }
     return state;
