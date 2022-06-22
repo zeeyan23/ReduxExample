703f8d181d24bae82a633c6433ff6344b66caa75
zeeyan23 <zeeyanraza444@gmail.com>
2022-06-22 16:52:54 +0530

    newadded

--- src/components/store/index.jsx
+++ src/components/store/index.jsx
@@ -1,40 +1,66 @@
 import {createStore} from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState={counter:0,showCounter:true}
-function counterReducer(state= initialState,action){
-    if(action.type==='increment'){
-        return{
-            counter:state.counter+1,
-            showCounter:state.showCounter
-        }
-    }
-    if(action.type==='increase'){
-        return{
-            counter:state.counter+action.amount,
-            showCounter:state.showCounter
-        }
-    }
-    if(action.type==='decrement'){
-        return{
-            counter:state.counter-1,
-            showCounter:state.showCounter
-        }
-    }
-    if(action.type==='decrease'){
-        return{
-            counter:state.counter-action.amount,
-            showCounter:state.showCounter
-        }
-    }
-    if(action.type==='toggle'){
-        return{
-            showCounter:!state.showCounter,
-            counter:state.counter
-        }
-    }
-    return state;
-}
 
-const store=createStore(counterReducer);
+const counterSlice=createSlice({
+    name: 'counter',
+    initialState,
+    reducers:{
+        increment(state) {
+            state.counter++;
+        },
+        increase(state,action) {
+            state.counter+action.amount;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        decrease(state,action) {
+            state.counter-action.amount;
+        },
+        toggle(state) {
+            state.showCounter=!state.showCounter;
+        }
+    }
+})
+
+const store=createStore(counterSlice.reducer);
 
 export default store;
+
+
+
+// function counterReducer(state= initialState,action){
+//     if(action.type==='increment'){
+//         return{
+//             counter:state.counter+1,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(action.type==='increase'){
+//         return{
+//             counter:state.counter+action.amount,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(action.type==='decrement'){
+//         return{
+//             counter:state.counter-1,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(action.type==='decrease'){
+//         return{
+//             counter:state.counter-action.amount,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(action.type==='toggle'){
+//         return{
+//             showCounter:!state.showCounter,
+//             counter:state.counter
+//         }
+//     }
+//     return state;
+// }
